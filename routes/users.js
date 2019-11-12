216cf188168b63467f242d2acda8ee711a2a6d74
Ian Baylis <ibaylis@USMDLT824.local>
2019-11-12 16:41:17 -0500

    Added auth routes and middleware

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require("express-validator");
 
 const User = require('../models/User');
 
@@ -46,14 +49,28 @@ router.post(
 
             await user.save();
 
-            res.send('User saved');
+            //res.send('User saved');
 
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+            {
+                expiresIn: 360000
+            },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
         }
-
     }
 );
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 app.use(express.json({ extended: false }))
 
 app.get('/', (req, res) => 
-    res.json({ msg: 'Welcome to the Cpontact Keeper API...' })
+    res.json({ msg: 'Welcome to the Contact Keeper API...' })
 );
 
 // Define Routes
