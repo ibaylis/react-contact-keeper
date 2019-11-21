d55918a54b77c703da3b1137f788aed5c337b50b
Ian Baylis <ibaylis@USMDLT824.local>
2019-11-21 17:27:13 -0500

    Adding authentication register and logins

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -43,15 +43,63 @@ const ContactState = props => {
                 phone: '444-444-4444',
                 type: 'personal'
             }
-        ]
+        ],
+        current: null,
+        filtered: null
     };
 
     const [state, dispatch] = useReducer(ContactReducer, initialState);
 
+    // Add Contact
+    const addContact = contact => {
+        contact.id = uuid.v4();
+        dispatch({ type: ADD_CONTACT, payload: contact });
+    };
+
+    // Delete Contact
+    const deleteContact = id => {
+        dispatch({ type: DELETE_CONTACT, payload: id });
+    }
+
+    // Set Current Contact
+    const setCurrent = contact => {
+        dispatch({ type: SET_CURRENT, payload: contact });
+    }
+
+
+    // Clear Current Contact
+    const clearCurrent = () => {
+        dispatch({ type: CLEAR_CURRENT });
+    }
+
+   // Set Update Contact
+   const updateContact = contact => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+}
+
+   // Set Filter Contacts
+   const filterContact = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+}
+
+   // Clear Filter
+   const clearFilter = () => {
+       dispatch({ type: CLEAR_FILTER });
+   }
+
     return (
         <ContactContext.Provider
             value={{
-                contacts: state.contacts
+                contacts: state.contacts,
+                current: state.current,
+                filtered: state.filtered,
+                addContact,
+                deleteContact,
+                setCurrent,
+                clearCurrent,
+                updateContact,
+                filterContact,
+                clearFilter
             }}
         >
             { props.children }
